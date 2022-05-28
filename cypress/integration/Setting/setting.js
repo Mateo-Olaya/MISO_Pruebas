85e5c6bd0bfc45acba7faf69178f993e205492c3
Mateo-Olaya <m.olaya11@uniandes.edu.co>
2022-05-28 10:29:15 -0500

    Se ajusta los pantallazos y features a probar

--- cypress/integration/Setting/setting.js
+++ cypress/integration/Setting/setting.js
@@ -124,401 +124,401 @@ When(`I expand title and description`, () => {
 
 When(`I expand language`, () => {
   commonSettings.clickExpandLanguage();
-  Cypress.env("vrtActive") && cy.screenshot("setting14");
+  Cypress.env("vrtActive") && cy.screenshot("setting15");
 })
 
 When(`I expand meta data`, () => {
   commonSettings.clickExpandMetadata();
-  Cypress.env("vrtActive") && cy.screenshot("setting14");
+  Cypress.env("vrtActive") && cy.screenshot("setting16");
 })
 
 When(`I expand twitter`, () => {
   commonSettings.clickExpandTwitter();
-  Cypress.env("vrtActive") && cy.screenshot("setting14");
+  Cypress.env("vrtActive") && cy.screenshot("setting17");
 })
 
 When(`I expand facebook`, () => {
   commonSettings.clickExpandFacebook();
-  Cypress.env("vrtActive") && cy.screenshot("setting14");
+  Cypress.env("vrtActive") && cy.screenshot("setting18");
 })
 
 When(`I expand social`, () => {
   commonSettings.clickExpandSocial();
-  Cypress.env("vrtActive") && cy.screenshot("setting14");
+  Cypress.env("vrtActive") && cy.screenshot("setting19");
 })
 
 When(`I clear title field`, () => {
   commonSettings.clearTitleField();
-  Cypress.env("vrtActive") && cy.screenshot("setting15");
+  Cypress.env("vrtActive") && cy.screenshot("setting20");
 })
 
 When(`I clear description field`, () => {
   commonSettings.clearDescriptionField();
-  Cypress.env("vrtActive") && cy.screenshot("setting15");
+  Cypress.env("vrtActive") && cy.screenshot("setting21");
 })
 
 When(`I clear language field`, () => {
   commonSettings.clearLanguageField();
-  Cypress.env("vrtActive") && cy.screenshot("setting15");
+  Cypress.env("vrtActive") && cy.screenshot("setting22");
 })
 
 When(`I clear meta data title field`, () => {
   commonSettings.clearMetadataTitleField();
-  Cypress.env("vrtActive") && cy.screenshot("setting15");
+  Cypress.env("vrtActive") && cy.screenshot("setting23");
 })
 
 When(`I clear meta data description field`, () => {
   commonSettings.clearMetadataDescriptionField();
-  Cypress.env("vrtActive") && cy.screenshot("setting15");
+  Cypress.env("vrtActive") && cy.screenshot("setting24");
 })
 
 When(`I clear twitter title field`, () => {
   commonSettings.clearTwitterTitleField();
-  Cypress.env("vrtActive") && cy.screenshot("setting15");
+  Cypress.env("vrtActive") && cy.screenshot("setting25");
 })
 
 When(`I clear twitter description field`, () => {
   commonSettings.clearTwitterDescriptionField();
-  Cypress.env("vrtActive") && cy.screenshot("setting15");
+  Cypress.env("vrtActive") && cy.screenshot("setting26");
 })
 
 When(`I clear facebook title field`, () => {
   commonSettings.clearFacebookTitleField();
-  Cypress.env("vrtActive") && cy.screenshot("setting15");
+  Cypress.env("vrtActive") && cy.screenshot("setting27");
 })
 
 When(`I clear facebook description field`, () => {
   commonSettings.clearFacebookDescriptionField();
-  Cypress.env("vrtActive") && cy.screenshot("setting15");
+  Cypress.env("vrtActive") && cy.screenshot("setting28");
 })
 
 When(`I clear social facebook field`, () => {
   commonSettings.clearSocialFacebookField();
-  Cypress.env("vrtActive") && cy.screenshot("setting15");
+  Cypress.env("vrtActive") && cy.screenshot("setting29");
 })
 
 When(`I clear social twitter field`, () => {
   commonSettings.clearSocialTwitterField();
-  Cypress.env("vrtActive") && cy.screenshot("setting15");
+  Cypress.env("vrtActive") && cy.screenshot("setting30");
 })
 
 When(`I clear navigation field`, () => {
   commonSettings.clearNavigationHomeField();
-  Cypress.env("vrtActive") && cy.screenshot("setting15");
+  Cypress.env("vrtActive") && cy.screenshot("setting31");
 })
 
 When(`I clear navigation url field`, () => {
   commonSettings.clearNavigationHomeUrlField();
-  Cypress.env("vrtActive") && cy.screenshot("setting15");
+  Cypress.env("vrtActive") && cy.screenshot("setting32");
 })
 
 When(`I clear secondary navigation field`, () => {
   commonSettings.clearNavigationSecondaryField();
-  Cypress.env("vrtActive") && cy.screenshot("setting15");
+  Cypress.env("vrtActive") && cy.screenshot("setting33");
 })
 
 When(`I clear secondary navigation url field`, () => {
   commonSettings.clearNavigationSecondaryUrlField();
-  Cypress.env("vrtActive") && cy.screenshot("setting15");
+  Cypress.env("vrtActive") && cy.screenshot("setting34");
 })
 
 When(`I save settings`, () => {
   commonSettings.saveSettings();
   cy.wait(500);
-  Cypress.env("vrtActive") && cy.screenshot("setting16");
+  Cypress.env("vrtActive") && cy.screenshot("setting35");
 })
 
 Then(`I see the settings saved message`, () => {
   commonSettings.isSaveButtonSuccess();
-  Cypress.env("vrtActive") && cy.screenshot("setting17"); 
+  Cypress.env("vrtActive") && cy.screenshot("setting36"); 
 })
 
 When(`I type a title with {string} characters`, (textLength) => {
   commonSettings.writeSiteTitle(randomString(textLength - 1));
-  Cypress.env("vrtActive") && cy.screenshot("setting18");
+  Cypress.env("vrtActive") && cy.screenshot("setting37");
 })
 
 When(`I type a description with {string} characters`, (textLength) => {
   commonSettings.writeSiteDescription(randomString(textLength - 1));
-  Cypress.env("vrtActive") && cy.screenshot("setting18");
+  Cypress.env("vrtActive") && cy.screenshot("setting38");
 })
 
 When(`I type a language with {string} characters`, (textLength) => {
   commonSettings.writeSiteLanguage(randomString(textLength - 1));
-  Cypress.env("vrtActive") && cy.screenshot("setting18");
+  Cypress.env("vrtActive") && cy.screenshot("setting39");
 })
 
 When(`I type a meta data title with {string} characters`, (textLength) => {
   commonSettings.writeSiteMetadataTitle(randomString(textLength - 1));
-  Cypress.env("vrtActive") && cy.screenshot("setting18");
+  Cypress.env("vrtActive") && cy.screenshot("setting40");
 })
 
 When(`I type a meta data description with {string} characters`, (textLength) => {
   commonSettings.writeSiteMetadataDescription(randomString(textLength - 1));
-  Cypress.env("vrtActive") && cy.screenshot("setting18");
+  Cypress.env("vrtActive") && cy.screenshot("setting41");
 })
 
 When(`I type a twitter title with {string} characters`, (textLength) => {
   commonSettings.writeSiteTwitterTitle(randomString(textLength - 1));
-  Cypress.env("vrtActive") && cy.screenshot("setting18");
+  Cypress.env("vrtActive") && cy.screenshot("setting42");
 })
 
 When(`I type a twitter description with {string} characters`, (textLength) => {
   commonSettings.writeSiteTwitterDescription(randomString(textLength - 1));
-  Cypress.env("vrtActive") && cy.screenshot("setting18");
+  Cypress.env("vrtActive") && cy.screenshot("setting43");
 })
 
 When(`I type a facebook title with {string} characters`, (textLength) => {
   commonSettings.writeSiteFacebookTitle(randomString(textLength - 1));
-  Cypress.env("vrtActive") && cy.screenshot("setting18");
+  Cypress.env("vrtActive") && cy.screenshot("setting44");
 })
 
 When(`I type a facebook description with {string} characters`, (textLength) => {
   commonSettings.writeSiteFacebookDescription(randomString(textLength - 1));
-  Cypress.env("vrtActive") && cy.screenshot("setting18");
+  Cypress.env("vrtActive") && cy.screenshot("setting45");
 })
 
 When(`I type a correct social facebook account`, () => {
   commonSettings.writeSiteSocialFacebook(data.facebook_url);
-  Cypress.env("vrtActive") && cy.screenshot("setting18");
+  Cypress.env("vrtActive") && cy.screenshot("setting46");
 })
 
 When(`I type a correct social twitter account`, () => {
   commonSettings.writeSiteSocialTwitter(data.twitter_url);
-  Cypress.env("vrtActive") && cy.screenshot("setting18");
+  Cypress.env("vrtActive") && cy.screenshot("setting47");
 })
 
 When(`I type an incorrect social facebook account`, () => {
   commonSettings.writeSiteSocialFacebook(data.facebook_url + data.description_page_invalid);
-  Cypress.env("vrtActive") && cy.screenshot("setting18");
+  Cypress.env("vrtActive") && cy.screenshot("setting48");
 })
 
 When(`I type an incorrect social twitter account`, () => {
   commonSettings.writeSiteSocialTwitter(data.twitter_url + data.description_page_invalid);
-  Cypress.env("vrtActive") && cy.screenshot("setting18");
+  Cypress.env("vrtActive") && cy.screenshot("setting49");
 })
 
 When(`I type a navigation with {string} characters`, (textLength) => {
   commonSettings.writeSiteNavigationHome(randomString(textLength - 1));
-  Cypress.env("vrtActive") && cy.screenshot("setting18");
+  Cypress.env("vrtActive") && cy.screenshot("setting50");
 })
 
 When(`I type a correct navigation url`, () => {
   commonSettings.writeSiteNavigationHomeUrl(Cypress.env("ghost_url"));
-  Cypress.env("vrtActive") && cy.screenshot("setting18");
+  Cypress.env("vrtActive") && cy.screenshot("setting51");
 })
 
 When(`I type an incorrect navigation url`, () => {
   commonSettings.writeSiteNavigationHomeUrl(Cypress.env("ghost_url") + data.description_page_invalid);
-  Cypress.env("vrtActive") && cy.screenshot("setting18");
+  Cypress.env("vrtActive") && cy.screenshot("setting52");
 })
 
 When(`I type a secondary navigation with {string} characters`, (textLength) => {
   commonSettings.writeSiteNavigationSecondary(randomString(textLength - 1));
-  Cypress.env("vrtActive") && cy.screenshot("setting18");
+  Cypress.env("vrtActive") && cy.screenshot("setting53");
 })
 
 When(`I type a correct secondary navigation url`, () => {
   commonSettings.writeSiteNavigationSecondaryUrl(Cypress.env("ghost_url"));
-  Cypress.env("vrtActive") && cy.screenshot("setting18");
+  Cypress.env("vrtActive") && cy.screenshot("setting54");
 })
 
 When(`I type an incorrect secondary navigation url`, () => {
   commonSettings.writeSiteNavigationSecondaryUrl(Cypress.env("ghost_url") + data.description_page_invalid);
-  Cypress.env("vrtActive") && cy.screenshot("setting18");
+  Cypress.env("vrtActive") && cy.screenshot("setting55");
 })
 
 When(`I focus in description field`, () => {
   commonSettings.focusDescriptionField();
-  Cypress.env("vrtActive") && cy.screenshot("setting19");
+  Cypress.env("vrtActive") && cy.screenshot("setting56");
 })
 
 When(`I focus in title field`, () => {
   commonSettings.focusTitleField();
-  Cypress.env("vrtActive") && cy.screenshot("setting19");
+  Cypress.env("vrtActive") && cy.screenshot("setting57");
 })
 
 When(`I focus in language field`, () => {
   commonSettings.focusLanguageField();
-  Cypress.env("vrtActive") && cy.screenshot("setting19");
+  Cypress.env("vrtActive") && cy.screenshot("setting58");
 })
 
 When(`I focus in meta data title field`, () => {
   commonSettings.focusMetadataTitleField();
-  Cypress.env("vrtActive") && cy.screenshot("setting19");
+  Cypress.env("vrtActive") && cy.screenshot("setting59");
 })
 
 When(`I focus in meta data description field`, () => {
   commonSettings.focusMetadataDescriptionField();
-  Cypress.env("vrtActive") && cy.screenshot("setting19");
+  Cypress.env("vrtActive") && cy.screenshot("setting60");
 })
 
 When(`I focus in twitter title field`, () => {
   commonSettings.focusTwitterTitleField();
-  Cypress.env("vrtActive") && cy.screenshot("setting19");
+  Cypress.env("vrtActive") && cy.screenshot("setting61");
 })
 
 When(`I focus in twitter description field`, () => {
   commonSettings.focusTwitterDescriptionField();
-  Cypress.env("vrtActive") && cy.screenshot("setting19");
+  Cypress.env("vrtActive") && cy.screenshot("setting62");
 })
 
 When(`I focus in facebook title field`, () => {
   commonSettings.focusFacebookTitleField();
-  Cypress.env("vrtActive") && cy.screenshot("setting19");
+  Cypress.env("vrtActive") && cy.screenshot("setting63");
 })
 
 When(`I focus in facebook description field`, () => {
   commonSettings.focusFacebookDescriptionField();
-  Cypress.env("vrtActive") && cy.screenshot("setting19");
+  Cypress.env("vrtActive") && cy.screenshot("setting64");
 })
 
 Then('I should see a title message error', () => {
   commonSettings.isTitleError();
-  Cypress.env("vrtActive") && cy.screenshot("setting19"); 
+  Cypress.env("vrtActive") && cy.screenshot("setting65"); 
 })
 
 Then('I should not see a title message error', () => {
   commonSettings.isNotTitleError();
-  Cypress.env("vrtActive") && cy.screenshot("setting19"); 
+  Cypress.env("vrtActive") && cy.screenshot("setting66"); 
 })
 
 Then('I should see a description message error', () => {
   commonSettings.isDescriptionError();
-  Cypress.env("vrtActive") && cy.screenshot("setting19"); 
+  Cypress.env("vrtActive") && cy.screenshot("setting67"); 
 })
 
 Then('I should not see a description message error', () => {
   commonSettings.isNotDescriptionError();
-  Cypress.env("vrtActive") && cy.screenshot("setting19"); 
+  Cypress.env("vrtActive") && cy.screenshot("setting68"); 
 })
 
 Then('I should see a language message error', () => {
   commonSettings.isLanguageError();
-  Cypress.env("vrtActive") && cy.screenshot("setting19"); 
+  Cypress.env("vrtActive") && cy.screenshot("setting69"); 
 })
 
 Then('I should not see a language message error', () => {
   commonSettings.isNotLanguageError();
-  Cypress.env("vrtActive") && cy.screenshot("setting19"); 
+  Cypress.env("vrtActive") && cy.screenshot("setting70"); 
 })
 
 Then('I should see a meta data title message error', () => {
   commonSettings.isMetadataTitleError();
-  Cypress.env("vrtActive") && cy.screenshot("setting19"); 
+  Cypress.env("vrtActive") && cy.screenshot("setting71"); 
 })
 
 Then('I should not see a meta data title message error', () => {
   commonSettings.isNotMetadataTitleError();
-  Cypress.env("vrtActive") && cy.screenshot("setting19"); 
+  Cypress.env("vrtActive") && cy.screenshot("setting72"); 
 })
 
 Then('I should see a meta data description message error', () => {
   commonSettings.isMetadataDescriptionError();
-  Cypress.env("vrtActive") && cy.screenshot("setting19"); 
+  Cypress.env("vrtActive") && cy.screenshot("setting73"); 
 })
 
 Then('I should not see a meta data description message error', () => {
   commonSettings.isNotMetadataDescriptionError();
-  Cypress.env("vrtActive") && cy.screenshot("setting19"); 
+  Cypress.env("vrtActive") && cy.screenshot("setting74"); 
 })
 
 Then('I should see a twitter title message error', () => {
   commonSettings.isTwitterTitleError();
-  Cypress.env("vrtActive") && cy.screenshot("setting19"); 
+  Cypress.env("vrtActive") && cy.screenshot("setting75"); 
 })
 
 Then('I should not see a twitter title message error', () => {
   commonSettings.isNotTwitterTitleError();
-  Cypress.env("vrtActive") && cy.screenshot("setting19"); 
+  Cypress.env("vrtActive") && cy.screenshot("setting76"); 
 })
 
 Then('I should see a twitter description message error', () => {
   commonSettings.isTwitterDescriptionError();
-  Cypress.env("vrtActive") && cy.screenshot("setting19"); 
+  Cypress.env("vrtActive") && cy.screenshot("setting77"); 
 })
 
 Then('I should not see a twitter description message error', () => {
   commonSettings.isNotTwitterDescriptionError();
-  Cypress.env("vrtActive") && cy.screenshot("setting19"); 
+  Cypress.env("vrtActive") && cy.screenshot("setting78"); 
 })
 
 Then('I should see a facebook title message error', () => {
   commonSettings.isFacebookTitleError();
-  Cypress.env("vrtActive") && cy.screenshot("setting19"); 
+  Cypress.env("vrtActive") && cy.screenshot("setting79"); 
 })
 
 Then('I should not see a facebook title message error', () => {
   commonSettings.isNotFacebookTitleError();
-  Cypress.env("vrtActive") && cy.screenshot("setting19"); 
+  Cypress.env("vrtActive") && cy.screenshot("setting80"); 
 })
 
 Then('I should see a facebook description message error', () => {
   commonSettings.isFacebookDescriptionError();
-  Cypress.env("vrtActive") && cy.screenshot("setting19"); 
+  Cypress.env("vrtActive") && cy.screenshot("setting81"); 
 })
 
 Then('I should not see a facebook description message error', () => {
   commonSettings.isNotFacebookDescriptionError();
-  Cypress.env("vrtActive") && cy.screenshot("setting19"); 
+  Cypress.env("vrtActive") && cy.screenshot("setting82"); 
 })
 
 Then('I should see a social facebook message error', () => {
   commonSettings.isSocialFacebookError();
-  Cypress.env("vrtActive") && cy.screenshot("setting19"); 
+  Cypress.env("vrtActive") && cy.screenshot("setting83"); 
 })
 
 Then('I should not see a social facebook message error', () => {
   commonSettings.isNotSocialFacebookError();
-  Cypress.env("vrtActive") && cy.screenshot("setting19"); 
+  Cypress.env("vrtActive") && cy.screenshot("setting84"); 
 })
 
 Then('I should see a social twitter message error', () => {
   commonSettings.isSocialTwitterError();
-  Cypress.env("vrtActive") && cy.screenshot("setting19"); 
+  Cypress.env("vrtActive") && cy.screenshot("setting85"); 
 })
 
 Then('I should not see a social twitter message error', () => {
   commonSettings.isNotSocialTwitterError();
-  Cypress.env("vrtActive") && cy.screenshot("setting19"); 
+  Cypress.env("vrtActive") && cy.screenshot("setting86"); 
 })
 
 Then('I should see a navigation message error', () => {
   commonSettings.isNavigationHomeError();
-  Cypress.env("vrtActive") && cy.screenshot("setting19"); 
+  Cypress.env("vrtActive") && cy.screenshot("setting87"); 
 })
 
 Then('I should not see a navigation message error', () => {
   commonSettings.isNotNavigationHomeError();
-  Cypress.env("vrtActive") && cy.screenshot("setting19"); 
+  Cypress.env("vrtActive") && cy.screenshot("setting88"); 
 })
 
 Then('I should see a navigation url message error', () => {
   commonSettings.isNavigationHomeUrlError();
-  Cypress.env("vrtActive") && cy.screenshot("setting19"); 
+  Cypress.env("vrtActive") && cy.screenshot("setting89"); 
 })
 
 Then('I should not see a navigation url message error', () => {
   commonSettings.isNotNavigationHomeUrlError();
-  Cypress.env("vrtActive") && cy.screenshot("setting19"); 
+  Cypress.env("vrtActive") && cy.screenshot("setting90"); 
 })
 
 Then('I should see a secondary navigation message error', () => {
   commonSettings.isNavigationSecondaryError();
-  Cypress.env("vrtActive") && cy.screenshot("setting19"); 
+  Cypress.env("vrtActive") && cy.screenshot("setting91"); 
 })
 
 Then('I should not see a secondary navigation message error', () => {
   commonSettings.isNotNavigationSecondaryError();
-  Cypress.env("vrtActive") && cy.screenshot("setting19"); 
+  Cypress.env("vrtActive") && cy.screenshot("setting92"); 
 })
 
 Then('I should see a secondary navigation url message error', () => {
   commonSettings.isNavigationSecondaryUrlError();
-  Cypress.env("vrtActive") && cy.screenshot("setting19"); 
+  Cypress.env("vrtActive") && cy.screenshot("setting93"); 
 })
 
 Then('I should not see a secondary navigation url message error', () => {
   commonSettings.isNotNavigationSecondaryUrlError();
-  Cypress.env("vrtActive") && cy.screenshot("setting19"); 
+  Cypress.env("vrtActive") && cy.screenshot("setting94"); 
 })
